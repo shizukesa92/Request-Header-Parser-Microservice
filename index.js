d9a97a7b6cbe921ecfeecddc931567210e9a9b7c
Leonard Tye <leonardtye1992@gmail.com>
2018-04-08 08:16:10 +0800

    Changed * to / in api

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const http = require("http");
 const api = require("./server/routes/api");
 
 app.use("/api/", api);
-
+app.use(express.static("client"));
 
 app.listen(process.env.PORT || 3000);
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("*", (req, res) => {
+router.get("/", (req, res) => {
 	res.status(200).send(req.protocol + '://' + req.get("host"));
 });
 module.exports = router;
